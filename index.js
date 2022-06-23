a20f38a5bc587bb65db2cf3f1b3e76b4bdf7f947
Luyecci <luuhtharion@gmail.com>
2022-06-23 14:11:08 +0200

    v0
    v0

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+
+
+// When the user put de code and 
+function acceder() {
+    
+
+}
+
+
+
+
+// Let the historical access showed on the screen
+function historial () {
+    
+}
+
+
+
+
+
+function registrar() {
+
+
+    
+}
